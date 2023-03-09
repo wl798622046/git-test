53f79ed30a7330dc9d08bd94b07c441d1f4cb46a
zwl <zwl@qq.com>
2023-03-09 23:26:06 +0800

    first commit

---
+++ src/main/java/com/cy/pj/GitTest.java
@@ -0,0 +1,12 @@
+package com.cy.pj;
+
+/**
+ * @author Zwlstart
+ * @date 2023/3/9 23:23
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
